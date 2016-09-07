9e1b6eb3893702fa6b9a41378748a71c3557257f
chester5009 <pauletto1234@gmail.com>
2016-09-07 04:52:28 +0300

    add personal cabinet

--- menu/profile.php
+++ menu/profile.php
@@ -6,14 +6,14 @@
 </head>
 <body>
 
-    <div id="info">
+    <form id="info">
         <a>Фото</a><br><img name="photo" src="" width="100" height="50"><br><br>
+        <input name="avatar" type="file"/><br><br>
         <a>Никнейм</a><input name="nick" id="nick"><br><br>
         <a>О себе</a><br><textarea name="about" id="about">about</textarea>
         <br><br>
         <button id="ok"  value="Сохранить изменения">Сохранить изменения</button>
-        <button id="cancel" value="Отмена">Отмена</button>
-    </div>
+    </form>
     <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
     <script src="js/profile.js"></script>
 </body>
